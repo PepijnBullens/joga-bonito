f8ac5618cd189cf459c6e5511b77b43397803062
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-03-25 20:29:37 +0100

    Set up foot movement

---
+++ code/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Game from "@/components/game";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
+      <Game />
+    </div>
+  );
+}
